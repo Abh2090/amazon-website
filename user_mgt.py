363a2452e44a6c96be6dfcabf8b067a45f38e631
Abhijit Herkal <abhijit.herkal@gmail.com>
2024-08-27 14:21:49 +0530

    Added user management functions in user_mgt.py

---
+++ user_mgt.py
@@ -0,0 +1,9 @@
+def add_user(username):
+    print(f"User{username} added successfully!")
+
+def delete_user(username):
+    print(f"User{username}deleted successfully!")
+
+    add_user("John_Doe")
+    delete_user("John_doe")
+
